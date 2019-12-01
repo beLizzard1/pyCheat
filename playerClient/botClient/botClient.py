870d26d71c2750f76096e9e68bb05f304239f351
Liz James <rjames93@sucs.org>
2019-12-01 11:06:28 +0000

    Bleh further reading of websockets and asyncio needed
    I need to figure out how these libraries work so that they can be incorporated into the Player class more easily

--- playerClient/botClient/botClient.py
+++ playerClient/botClient/botClient.py
@@ -3,21 +3,19 @@ import websockets
 
 class Player():
 
-    def __init__(self, uri="ws://localhost:8765", playerNumber):
+    def __init__(self,  playerNumber, uri="ws://localhost:8765"):
         # Set important variables
         self.uri = uri
         self.playerNumber = playerNumber
         
         # Try register the player with the server
-        if ( registerPlayer() == False )
+        if ( asyncio.run( registerPlayer() ) == False ):
             print("Init of Player Failed")
             return
         
         # So we have registered as Player {playerNumber} so we are done for now :)
         
-        
-
-    def registerPlayer(self):
+    async def registerPlayer(self):
         async with websockets.connect(uri) as websocket:
             
             registerMessage = "I want to be Player {}".format(playerNumber)
